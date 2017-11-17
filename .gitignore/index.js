d5a4a3f626fcb0b260affc1819e87be92d9d639b
Azecko <30987143+Azecko@users.noreply.github.com>
2017-11-17 09:23:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,6 +4,19 @@ const TOKEN = "Mzc4Mjk2NTYxMDYwMjgyMzY4.DOuWIw.KBrhOFO0ZQ30sTrSG8v9sJxsE48";
 const PREFIX = "z!";
 const EVERYONE = "@";
 
+function play(connection, message) {
+    var server = servers[message.guild.id];
+
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+    server.queue.shift();
+
+    server.dispatcher.on("end", function() {
+        if (server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    });
+}
+
 var client = new Discord.Client();
 
 var bot = new Discord.Client();
