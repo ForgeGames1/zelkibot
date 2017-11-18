cc932e11c7193937364d3c293e64bff172d553ee
XeCrafT <32407457+XeCrafT@users.noreply.github.com>
2017-11-17 19:08:10 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -405,14 +405,6 @@ bot.on("message", function(message) {
     let suffix = message.content.split(" ")[1];
     let player = bot.voiceConnections.get('server', message.server);
     if(!player || !player.playing) return bot.sendMessage(message, 'Aucune musique ne joue en ce moment');
-    if(!suffix) {
-        bot.sendMessage(message, `Le volume est ${(player.getVolume() * 50)}`);
-        let volumeBefore = player.getVolume();
-        let volume = parseInt(suffix);
-        if(volume > 50) return bot.sendMessage(message, "Sa sert à rien de mettre 50% vous aller plus attendre vos amis parler!");
-        player.setVolume((volume / 50));
-        bot.sendMessage(message, `Le volume est passé de ${(volumeBefore * 50)} à ${volume}`);
-               }
             break;
             case "pause":      
             var server = servers[message.guild.id];
